1c443700731555be21056b955a584e0bc42a17e4
Rakibul Yeasin <ryeasin03@gmail.com>
2019-10-25 23:19:05 +0600

    Chabged Requirements

--- result/result.py
+++ result/result.py
@@ -22,10 +22,10 @@ def result(reg):
         # return name, regn, college
         # return data
         # return name, regn, college, sems
-        return [{
-            "text": f"{name}\n{regn}\n{college}\n{sems}"
-        }]
-        # return name
+        # return [{
+        #     "text": f"{name}\n{regn}\n{college}\n{sems}"
+        # }]
+        return name
     except IndexError:
         print("Reg not Found!")
         pass
